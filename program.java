9a07d6fcbb1439645d168072c8c8d4c7da54e9c8
AnjaSamsom <anjavt14@gmail.com>
2023-04-06 20:39:15 -0400

    commented, made sure it works

--- program.java
+++ program.java
@@ -13,7 +13,7 @@ public class program
 
    public static void main(String[]args)
    {
-      int W = 11;
+      // list of weights
       listW.add(0);
       listW.add(1);
       listW.add(2);
@@ -21,8 +21,7 @@ public class program
       listW.add(6);
       listW.add(7);
 
-
-
+      // list of values
       listV.add(0);
       listV.add(1);
       listV.add(6);
@@ -31,8 +30,16 @@ public class program
       listV.add(28); 
 
 
-      System.out.println(knapsack(W, listW, listV));
+      // change this value to 10
+      int W = 11;
+      System.out.println("W = " + W);
+      System.out.println("Value is " + knapsack(W, listW, listV));
+      System.out.print("The set of items is ");
+      traceback(5, W);
    }
+
+   public static void run_methods(int W)
+   {}
 
    public static int knapsack(int W, ArrayList<Integer> listW, ArrayList<Integer> listV)
    {
@@ -50,14 +57,18 @@ public class program
          }
       } 
 
+      /// declaring a lot of variables to fill later so it is easier
       int vi;
       int wi;
       int a;
       int b;
       int max;
+
+      // loop through i
       for(int i = 1; i<n; i++)
       {
 
+         // loop through w
          for(w=0; w<=W; w++)
          {
             if(listW.get(i)>w)
@@ -69,6 +80,7 @@ public class program
                vi = listV.get(i);
                wi = listW.get(i);
 
+               // find maximum of these two terms
                a = (M.get(i-1).get(w));
                b = (vi + M.get(i-1).get(w-wi));
 
@@ -77,14 +89,11 @@ public class program
                else
                   max = b;
 
-               
                M.get(i).set(w, max);
             }
 
-
-
          }
-         System.out.println("new table:");
+         System.out.println("memoization table:");
          for(ArrayList<Integer> my_list : M)
          {
             System.out.println(my_list);
@@ -105,23 +114,24 @@ public class program
       int vi = listV.get(i);
 
       if(i == 0)
+      {
          System.out.println();
+         return 0;
+      }
+
 
       if(wi > w)
          return traceback(i-1, w);
 
       else if(vi + M.get(i-1).get(w-wi) > M.get(i-1).get(w))
       {
-         System.out.println(i);
+         System.out.print(i + " " );
          return traceback(i-1, w-wi);
       }
 
       else
          return traceback(i-1, w);
-      
-
-
-      
+         
    }
 
 }
